5d1d9617ff793b5aac61a3a00a63ffa7f79c2de0
Aman <amankunda9@gmail.com>
2022-11-12 01:00:08 +0530

    added react router dom

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Categories from "./pages/Categories";
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-    </div>
+    <>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/general" element={<Categories />} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,14 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <div>
       <nav class="navbar navbar-expand-lg bg-dark">
         <div class="container-fluid">
-          <a class="navbar-brand text-white" href="#">
+          <Link class="navbar-brand text-white" to="/">
             <h5>NEWSERY</h5>
-          </a>
+          </Link>
           <button
             class="navbar-toggler"
             type="button"
@@ -22,76 +23,76 @@ const Navbar = () => {
           <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
               <li class="nav-item">
-                <a
+                <Link
                   class="nav-link active text-white"
                   aria-current="page"
-                  href="#"
+                  to="/"
                 >
                   Home
-                </a>
+                </Link>
               </li>
               <li class="nav-item">
-                <a
+                <Link
                   class="nav-link active text-white"
                   aria-current="page"
-                  href="#"
+                  to="/general"
                 >
                   General
-                </a>
+                </Link>
               </li>
               <li class="nav-item">
-                <a
+                <Link
                   class="nav-link active text-white"
                   aria-current="page"
-                  href="#"
+                  to="/business"
                 >
                   Business
-                </a>
+                </Link>
               </li>
               <li class="nav-item">
-                <a
+                <Link
                   class="nav-link active text-white"
                   aria-current="page"
-                  href="#"
+                  to="/entertainment"
                 >
                   Entertainment
-                </a>
+                </Link>
               </li>
               <li class="nav-item">
-                <a
+                <Link
                   class="nav-link active text-white"
                   aria-current="page"
-                  href="#"
+                  to="/health"
                 >
                   Health
-                </a>
+                </Link>
               </li>
               <li class="nav-item">
-                <a
+                <Link
                   class="nav-link active text-white"
                   aria-current="page"
-                  href="#"
+                  to="/science"
                 >
                   Science
-                </a>
+                </Link>
               </li>
               <li class="nav-item">
-                <a
+                <Link
                   class="nav-link active text-white"
                   aria-current="page"
-                  href="#"
+                  to="/sports"
                 >
                   Sports
-                </a>
+                </Link>
               </li>
               <li class="nav-item">
-                <a
+                <Link
                   class="nav-link active text-white"
                   aria-current="page"
-                  href="#"
+                  to="/terminology"
                 >
                   Technology
-                </a>
+                </Link>
               </li>
             </ul>
           </div>
