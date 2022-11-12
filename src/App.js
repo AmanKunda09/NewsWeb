d9c9f37734d28879a3b8bd21890e75000c35d4eb
Aman <amankunda9@gmail.com>
2022-11-13 02:41:52 +0530

    added footer

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Categories from "./pages/Categories";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -38,6 +39,7 @@ function App() {
             element={<Categories cat={"technology"} />}
           />
         </Routes>
+        <Footer />
       </Router>
     </>
   );
---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="container-fluid d-flex align-items-center justify-content-center bg-dark text-white p-3">
+      <h5>
+        Developed with love by <i>Aman Kunda</i>
+      </h5>
+    </div>
+  );
+};
+
+export default Footer;
